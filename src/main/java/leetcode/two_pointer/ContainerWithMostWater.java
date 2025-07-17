deb274f1a7af7c8ef0432c3c12eb63e3459b8230
k0m010t <krishnendu.moahapatra@walmart.com>
2025-07-17 18:05:11 +0530

    update two pointer code

--- src/main/java/leetcode/two_pointer/ContainerWithMostWater.java
+++ src/main/java/leetcode/two_pointer/ContainerWithMostWater.java
@@ -1,7 +1,24 @@
 package leetcode.two_pointer;
 
 public class ContainerWithMostWater {
+
     public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        int maxArea = 0;
+        while(left < right){
+            if(height[left] <= height[right]){
+                maxArea = Math.max(maxArea,(right-left) * height[left]);
+                left++;
+            }else {
+                maxArea = Math.max(maxArea, (right-left) * height[right]);
+                right --;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public int maxAreaMinimal(int[] height) {
         int lo = 0, hi = height.length - 1, max = 0;
         while (lo < hi) {
             int area = (hi - lo) * Math.min(height[lo], height[hi]);
